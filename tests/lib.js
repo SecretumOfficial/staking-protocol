a59f1b71eec49abcc338fc74dfa1bb7b2101c160
yufu <yufu090625@gmail.com>
2022-03-21 16:32:05 +0800

    implement tests

---
+++ tests/lib.js
@@ -0,0 +1,334 @@
+const anchor = require('@project-serum/anchor');
+const { TOKEN_PROGRAM_ID } = require('@solana/spl-token');
+const utils = require('../lib/utils');
+
+function formatError(errors, err) {
+    if (err.InstructionError !== null && err.InstructionError.length === 2) {
+        const errCode = err.InstructionError[1].Custom;
+        if (errCode >= errors[0].code && errCode <= errors[errors.length - 1].code) {
+            return errors[errCode - errors[0].code].msg;
+        }
+        return `Custom erro code= ${errCode}`;
+    }
+    console.log(err);
+    return 'unknown error';
+}
+
+function parseErrorNumber(errors, logs) {
+    const key = 'Program log: Custom program error: ';
+    for (let i = 0; i < logs.length; i++) {
+        if (logs[i].indexOf(key) >= 0) {
+            const numStr = logs[i].substring(key.length);
+            const errorNum = Number(numStr);
+            const idx = errorNum % 100;
+            return errors[idx].msg;
+        }
+    }
+    return undefined;
+}
+
+
+async function initialize(
+    program,
+    funderAuthority,
+    mintAddress,
+    apyMax,
+    minTimeframeInSecond,
+    signer,
+) {
+    const stakingDataAccount = await utils.getStakingDataAccount(signer.publicKey, mintAddress, program.programId);
+    const escrowAccount = await utils.getEscrowAccount(stakingDataAccount, program.programId);
+    const rewarderAccount = await utils.getRewarderAccount(stakingDataAccount, program.programId);
+    let result;
+    try{
+        await program.rpc.initialize(
+            apyMax,
+            new anchor.BN(minTimeframeInSecond),
+            {
+                accounts: {
+                    stakingData: stakingDataAccount,
+                    funderAuthority: funderAuthority,
+                    escrowAccount: escrowAccount,
+                    rewarderAccount: rewarderAccount,
+                    authority: signer.publicKey,
+                    mintAddress: mintAddress,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                    rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+                },
+                signers: [signer],
+            },
+        );
+        result = stakingDataAccount;
+    } catch(e)    {
+        if (e.msg === undefined) {
+            result = parseErrorNumber(program._idl.errors, e.logs);
+        } else result = e.msg;
+    }
+    return stakingDataAccount;
+}
+
+async function initializeStakeState(
+    program,
+    stakingDataAccount,
+    signer,
+) {
+    const stakingData = await utils.getStakingData(program, stakingDataAccount);
+    if(stakingData == null)
+    {
+        return "didn't init staking";
+    }
+    const stakeStateAccount = await utils.getStakingStateAccount(stakingDataAccount, signer.publicKey, program.programId);
+
+    let result;
+    try{
+        await program.rpc.initializeStakeState(
+            {
+                accounts: {
+                    stakingData: stakingDataAccount,
+                    stakeStateAccount: stakeStateAccount,
+                    authority: signer.publicKey,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                    rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+                },
+                signers: [signer],
+            },
+        );    
+        result = stakeStateAccount;
+    }catch(e)
+    {
+        if (e.msg === undefined) {
+            result = parseErrorNumber(program._idl.errors, e.logs);
+        } else result = e.msg;
+    }
+    return result;
+}
+
+
+async function staking(
+    program,
+    stakingDataAccount,
+    stakerAccount,
+    amount,
+    signer,
+) {
+    const stakingData = await utils.getStakingData(program, stakingDataAccount);
+    if(stakingData == null)
+    {
+        return "stakingData didn't init";
+    }
+    const stakeStateAccount = await utils.getStakingStateAccount(stakingDataAccount, signer.publicKey, program.programId);
+    const stakeState = await utils.getStakingState(program, stakeStateAccount);
+    if(stakeState == null)
+    {
+        return "stakingState didn't init";
+    }
+    let result;
+
+    try{
+        await program.rpc.staking(
+            new anchor.BN(amount),
+            {
+                accounts: {
+                    stakingData: stakingDataAccount,
+                    stakeStateAccount: stakeStateAccount,
+                    escrowAccount: stakingData.escrowAccount,
+                    stakerAccount: stakerAccount,
+                    authority: signer.publicKey,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                },
+                signers: [signer],
+            },
+        );
+        result = amount;
+    }catch(e)    {
+        if (e.msg === undefined) {
+            result = parseErrorNumber(program._idl.errors, e.logs);
+        } else result = e.msg;
+    }
+    return result;
+}
+
+
+async function unstaking(
+    program,
+    stakingDataAccount,
+    reclaimer,
+    amount,
+    signer,
+) {
+    const stakingData = await utils.getStakingData(program, stakingDataAccount);
+    if(stakingData == null)
+    {
+        return "stakingData didn't init";
+    }
+    const stakeStateAccount = await utils.getStakingStateAccount(stakingDataAccount, signer.publicKey, program.programId);
+    const stakeState = await utils.getStakingState(program, stakeStateAccount);
+    if(stakeState == null)
+    {
+        return "stakingState didn't init";
+    }
+
+    const stakingAuthority = await utils.getStakingAuthAccount(stakingDataAccount, program.programId);
+    let result;
+    try{
+        await program.rpc.unstaking(
+            new anchor.BN(amount),
+            {
+                accounts: {
+                    stakingData: stakingDataAccount,
+                    stakeStateAccount: stakeStateAccount,
+                    escrowAccount: stakingData.escrowAccount,
+                    reclaimer: reclaimer,
+                    authority: signer.publicKey,
+                    stakingAuthority: stakingAuthority,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                },
+                signers: [signer],
+            },
+        );   
+        result = amount; 
+    }catch(e){
+        if (e.msg === undefined) {
+            result = parseErrorNumber(program._idl.errors, e.logs);
+        } else result = e.msg;
+    }
+    return result;
+}
+
+async function claimReward(
+    program,
+    stakingDataAccount,
+    claimer,
+    amount,
+    signer,
+) {
+    const stakingData = await utils.getStakingData(program, stakingDataAccount);
+    if(stakingData == null)
+    {
+        return "stakingData didn't init";
+    }
+    const stakeStateAccount = await utils.getStakingStateAccount(stakingDataAccount, signer.publicKey, program.programId);
+    const stakeState = await utils.getStakingState(program, stakeStateAccount);
+    if(stakeState == null)
+    {
+        return "stakingState didn't init";
+    }
+
+    const stakingAuthority = await utils.getStakingAuthAccount(stakingDataAccount, program.programId)
+    let result;
+    try{
+        await program.rpc.claimReward(
+            new anchor.BN(amount),
+            {
+                accounts: {
+                    stakingData: stakingDataAccount,
+                    stakeStateAccount: stakeStateAccount,
+                    rewarderAccount: stakingData.rewarderAccount,
+                    claimer: claimer,
+                    authority: signer.publicKey,
+                    stakingAuthority: stakingAuthority,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                },
+                signers: [signer],
+            },
+        );    
+        result = amount;
+    } catch (e){
+        if (e.msg === undefined) {
+            result = parseErrorNumber(program._idl.errors, e.logs);
+        } else result = e.msg;
+    }
+    return result;
+}
+
+async function funding(
+    program,
+    stakingDataAccount,
+    funderAccount,
+    amount,
+    timeframeInSecond,
+    signer,
+) {
+    const stakingData = await utils.getStakingData(program, stakingDataAccount);
+    if(stakingData == null)
+    {
+        return "stakingData didn't init";
+    }
+
+    let result;
+    try{
+        await program.rpc.funding(
+            new anchor.BN(amount),
+            new anchor.BN(timeframeInSecond),
+            {
+                accounts: {
+                    stakingData: stakingDataAccount,
+                    rewarderAccount: stakingData.rewarderAccount,
+                    funderAccount: funderAccount,
+                    authority: signer.publicKey,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                },
+                signers: [signer],
+            },
+        );    
+        result = amount;
+    }catch(e){
+        console.log(e);
+        if (e.msg === undefined) {
+            result = parseErrorNumber(program._idl.errors, e.logs);
+        } else result = e.msg;        
+    }
+    return result;
+}
+
+function calculateReward(apyMax, poolStaked, poolReward, timeFrameStart, timeFrameEnd, staked, stakedTime)
+{
+    if (staked === 0 || stakedTime >= timeFrameEnd){
+        return 0;
+    }
+
+    let seconds = timeFrameEnd - timeFrameStart;
+    if (stakedTime > timeFrameStart) {
+        seconds = timeFrameEnd - stakedTime;
+    }
+    let days = seconds/ (3600 * 24);
+    let frameDays = seconds / (3600 * 24);
+    let gainedTotal = (poolReward * days * staked)/ (frameDays * poolStaked);
+    let gainedPerDay = gainedTotal / days;
+    let stakedPerDay = staked / days;
+    let gainedPercentPerDay = gainedPerDay * 100.00 / stakedPerDay;
+    let apdMax = apyMax / 365.50;
+
+    if (gainedPercentPerDay > apdMax){
+        gainedPercentPerDay = apdMax;
+    }
+
+    let gained = gainedPercentPerDay * stakedPerDay * days / 100.00;
+    return gained;
+}
+
+function getGainedReward(stakingData, stakingState)
+{
+    for(let i=0; i<stakingData.stakers.length; i++)
+    {
+        const staker = stakingData.stakers[i];
+        if(staker.stakerCrc !== stakingState.myCrc)
+            continue;
+        return staker.gainedReward.toNumber();
+    }
+    return 0;
+}
+
+module.exports = {
+    initialize,   
+    initializeStakeState,
+    staking,
+    unstaking,
+    claimReward,
+    funding,
+    calculateReward,
+    getGainedReward
+}
+
---
+++ tests/staking/claiming.test.js
@@ -0,0 +1,193 @@
+const utils = require('../../lib/utils');
+
+const assert = require('assert');
+const anchor = require('@project-serum/anchor');
+const splToken = require('@solana/spl-token');
+const process = require('process');
+const os = require('os');
+const fs = require('fs');
+const lib = require('../lib');
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
+}
+  
+
+describe('Claiming tests', () => {
+    const homedir = os.homedir();
+    process.env.ANCHOR_WALLET = `${homedir}/.config/solana/id.json`;
+
+    // Configure the local cluster.
+    const provider = anchor.Provider.local();
+    anchor.setProvider(provider);
+
+    // Read the generated IDL.
+    const idl = JSON.parse(fs.readFileSync('./target/idl/staking.json', 'utf8'));
+
+    // Address of the deployed program.
+    const programId = new anchor.web3.PublicKey(idl.metadata.address);
+    const program = new anchor.Program(idl, programId);
+    const mintAuthority = anchor.web3.Keypair.generate();
+
+    let mint;
+    let stakingInitializer;
+    let stakerInitializer;
+    let stakerAccount;
+    let stakingDataAccount;
+
+    let funderAuthority;
+    let funderAccount;
+    let minTimeframeInSecond = 30;
+
+    beforeEach(async () => {
+        // create wallet A
+        stakingInitializer = anchor.web3.Keypair.generate();
+        await utils.createWallet(provider.connection, stakingInitializer.publicKey, 1000_000_000);
+        mint = await splToken.Token.createMint(
+            provider.connection,
+            stakingInitializer,
+            mintAuthority.publicKey,
+            null,
+            0,
+            splToken.TOKEN_PROGRAM_ID,
+        );
+
+        //init funder
+        funderAuthority = anchor.web3.Keypair.generate();
+        await utils.createWallet(provider.connection, funderAuthority.publicKey, 1000_000_000);         
+        funderAccount = await mint.createAccount(funderAuthority.publicKey);
+        await mint.mintTo(funderAccount, mintAuthority.publicKey, [mintAuthority], 100_000_000_000);
+
+        //init staker
+        stakerInitializer = anchor.web3.Keypair.generate();
+        await utils.createWallet(provider.connection, stakerInitializer.publicKey, 1000_000_000);
+        stakerAccount = await mint.createAccount(stakerInitializer.publicKey);        
+        await mint.mintTo(stakerAccount, mintAuthority.publicKey, [mintAuthority], 100_000_000_000);
+
+        //init staking
+        stakingDataAccount = await lib.initialize(program, funderAuthority.publicKey, mint.publicKey, 800, minTimeframeInSecond, stakingInitializer);
+
+        //init staker state
+        const stakerStateAccount = await lib.initializeStakeState(program, stakingDataAccount, stakerInitializer);
+        const stakerState = await utils.getStakingState(program, stakerStateAccount);
+        assert(stakerState.stakingAccount.toBase58() === stakingDataAccount.toBase58());
+        assert(stakerState.mintAddress.toBase58() === mint.publicKey.toBase58());
+        assert(stakerState.onwerAddress.toBase58() === stakerInitializer.publicKey.toBase58());
+    });
+
+    it.skip('Claiming for not gained', async () => {
+        //first staking
+        const amount = 1000;
+        const escrowAccount = await utils.getEscrowAccount(stakingDataAccount, program.programId);
+        const escrowAccountBalance = await utils.getTokenAccountBalance(program.provider.connection, escrowAccount);
+        const stakerAccountBalance = await utils.getTokenAccountBalance(program.provider.connection, stakerAccount);        
+        await lib.staking(program, stakingDataAccount, stakerAccount, amount, stakerInitializer);
+
+        const escrowAccountBalance1 = await utils.getTokenAccountBalance(program.provider.connection, escrowAccount);
+        const stakerAccountBalance1 = await utils.getTokenAccountBalance(program.provider.connection, stakerAccount);
+        assert(escrowAccountBalance1 === escrowAccountBalance + amount);
+        assert(stakerAccountBalance1 === stakerAccountBalance - amount);
+
+        const stakingData = await utils.getStakingData(program, stakingDataAccount);
+        assert(stakingData.totalStaked.toNumber() === amount);
+        
+        //try claiming
+        res = await lib.claimReward(program, stakingDataAccount, stakerAccount, 100, stakerInitializer);
+        assert(res === 'insufficient gained reward')
+        
+    });
+
+    it.skip('Claiming full amount', async () => {
+        //first staking
+        const amount = 1000;
+        const escrowAccount = await utils.getEscrowAccount(stakingDataAccount, program.programId);
+        const escrowAccountBalance = await utils.getTokenAccountBalance(program.provider.connection, escrowAccount);
+        const stakerAccountBalance = await utils.getTokenAccountBalance(program.provider.connection, stakerAccount);        
+        await lib.staking(program, stakingDataAccount, stakerAccount, amount, stakerInitializer);
+
+        const escrowAccountBalance1 = await utils.getTokenAccountBalance(program.provider.connection, escrowAccount);
+        const stakerAccountBalance1 = await utils.getTokenAccountBalance(program.provider.connection, stakerAccount);
+        assert(escrowAccountBalance1 === escrowAccountBalance + amount);
+        assert(stakerAccountBalance1 === stakerAccountBalance - amount);
+
+        let stakingData = await utils.getStakingData(program, stakingDataAccount);
+        assert(stakingData.totalStaked.toNumber() === amount);
+
+        //funding
+        const fundAmount = 1000;
+        const timeframeInSecond = 30;
+        await lib.funding(program, stakingDataAccount, funderAccount, fundAmount, timeframeInSecond, funderAuthority);
+        stakingData = await utils.getStakingData(program, stakingDataAccount);
+
+        //sleep 32 second call funding again
+        console.log("waiting 32 seconds");
+        await sleep(32000);
+
+        await lib.funding(program, stakingDataAccount, funderAccount, fundAmount, timeframeInSecond, funderAuthority);
+        stakingData = await utils.getStakingData(program, stakingDataAccount);
+        const gainedReward = stakingData.stakers[0].gainedReward.toNumber();
+        assert(stakingData.payoutReward.toNumber() === gainedReward);
+
+        console.log("claiming gained reward=", gainedReward);
+        const res = await lib.claimReward(program, stakingDataAccount, stakerAccount, gainedReward, stakerInitializer);
+  
+        const stakerAccountBalance2 = await utils.getTokenAccountBalance(program.provider.connection, stakerAccount);
+        assert(stakerAccountBalance2 === stakerAccountBalance1 + gainedReward);
+        stakingData = await utils.getStakingData(program, stakingDataAccount);
+
+        console.log(stakingData);
+
+        assert(stakingData.totalRewardPaid.toNumber() === gainedReward);
+        assert(stakingData.payoutReward.toNumber() === 0);
+        assert(stakingData.stakers[0].gainedReward.toNumber() === 0);
+
+    });
+
+    it('Claiming some amount', async () => {
+        //first staking
+        const amount = 1000;
+        const escrowAccount = await utils.getEscrowAccount(stakingDataAccount, program.programId);
+        const escrowAccountBalance = await utils.getTokenAccountBalance(program.provider.connection, escrowAccount);
+        const stakerAccountBalance = await utils.getTokenAccountBalance(program.provider.connection, stakerAccount);        
+        await lib.staking(program, stakingDataAccount, stakerAccount, amount, stakerInitializer);
+
+        const escrowAccountBalance1 = await utils.getTokenAccountBalance(program.provider.connection, escrowAccount);
+        const stakerAccountBalance1 = await utils.getTokenAccountBalance(program.provider.connection, stakerAccount);
+        assert(escrowAccountBalance1 === escrowAccountBalance + amount);
+        assert(stakerAccountBalance1 === stakerAccountBalance - amount);
+
+        let stakingData = await utils.getStakingData(program, stakingDataAccount);
+        assert(stakingData.totalStaked.toNumber() === amount);
+
+        //funding
+        const fundAmount = 1000;
+        const timeframeInSecond = 30;
+        await lib.funding(program, stakingDataAccount, funderAccount, fundAmount, timeframeInSecond, funderAuthority);
+        stakingData = await utils.getStakingData(program, stakingDataAccount);
+
+        //sleep 32 second call funding again
+        console.log("waiting 32 seconds");
+        await sleep(32000);
+
+        await lib.funding(program, stakingDataAccount, funderAccount, fundAmount, timeframeInSecond, funderAuthority);
+        stakingData = await utils.getStakingData(program, stakingDataAccount);
+        const gainedReward = stakingData.stakers[0].gainedReward.toNumber();
+        assert(stakingData.payoutReward.toNumber() === gainedReward);
+
+        console.log("claiming gained reward=", 1);
+        const res = await lib.claimReward(program, stakingDataAccount, stakerAccount, 1, stakerInitializer);
+  
+        const stakerAccountBalance2 = await utils.getTokenAccountBalance(program.provider.connection, stakerAccount);
+        assert(stakerAccountBalance2 === stakerAccountBalance1 + 1);
+        stakingData = await utils.getStakingData(program, stakingDataAccount);
+
+        assert(stakingData.totalRewardPaid.toNumber() === 1);
+        assert(stakingData.payoutReward.toNumber() === gainedReward - 1);
+        assert(stakingData.stakers[0].gainedReward.toNumber() === gainedReward - 1);
+
+        const reward = lib.calculateReward(800, 1000, 1000, 0, 30, 1000, 0);
+        console.log("reward=", reward);
+    });
+})
